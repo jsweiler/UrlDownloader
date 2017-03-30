ada638bded47173d8f0fdb72bcd49d50b350f83a
Jeff Weiler <jeffweiler8770@gmail.com>
2017-03-30 10:04:41 -0400

    intial creation

---
+++ UrlDownloader/Url.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlDownloader
+{
+    public class Url
+    {
+        public async Task<string> DownloadStringAsync(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.GetAsync(url);
+                return await result.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
